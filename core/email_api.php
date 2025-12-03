6f730d0c140f4b431aa703cb0baf7e758eb558d8
Veselin Malezanov <veselin@lukanet.com>
2025-12-03 15:18:30 +0200

    Промени по email API

--- core/email_api.php
+++ core/email_api.php
@@ -1810,7 +1810,10 @@ function email_bug_info_to_one_user( array $p_visible_bug_data, string $p_messag
 	# build message
 	$t_message = lang_get_defaulted( $p_message_id );
 
-	if( $p_header_optional_params ) {
+
+        $t_message .= event_signal( 'EVENT_BEFORE_COMPOSE_EMAIL', array( 'message_id' => $p_message_id, 'params' => $p_visible_bug_data ) );
+
+	if( is_array( $p_header_optional_params ) ) {
 		$t_message = vsprintf( $t_message, $p_header_optional_params );
 	}
 
